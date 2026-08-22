572d3698689e902c40e8301dc9d7f3fb42eec9fd
simonfab18 <simognf@gmail.com>
2026-08-22 20:07:15 +0800

    Resume Feedback Page

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,5 @@ export default [
     index("routes/home.tsx"),
     route('/auth', 'routes/auth.tsx'),
     route('/Upload', 'routes/Upload.tsx'),
+    route('/resume/:id', 'routes/resume.tsx'),
 ] satisfies RouteConfig;
